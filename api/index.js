849e9b3d890afea08c7f94e967322d43878f7805
KingsIPTV7 <173617250+KingsIPTV7@users.noreply.github.com>
2025-05-31 13:20:35 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ api/index.js
@@ -0,0 +1,15 @@
+export default function handler(req, res) {
+  const { id } = req.query;
+
+  if (!id) {
+    // Redirect to Telegram link if no ID provided
+    res.writeHead(302, { Location: 'https://t.me/HTLivesm3u' });
+    res.end();
+    return;
+  }
+
+  // Redirect to the manifest.mpd with the provided ID
+  const target = `http://amit-1.allinonereborn.in/tplaydrm333/manifest.mpd?id=${id}`;
+  res.writeHead(302, { Location: target });
+  res.end();
+}
